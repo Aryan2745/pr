c71dcd15b658b8e516604810d9d8b348dde48042
Aryan Kumar <Kr.aryan2745@gmail.com>
2024-06-08 09:15:28 +0530

    adad

---
+++ arrmax.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<limits.h>
+using namespace std; 
+int main()
+{  
+    int arr[100]; 
+    int n; 
+    cout<< " size of array:" ; 
+    cin>>n; 
+    cout<< " enter the elements of the array: ";  
+    for(int i =0; i<n;i++)
+    { 
+        cin>>arr[i];
+    }
+     // Max number in an array
+    //  int maxnum = INT_MIN;
+    // int arr[7]={1,2,3,89,3,9,2}; 
+    // for(int i=0;i<7;i++){  
+    // if( arr[i]>maxnum)
+    // maxnum= arr[i]; 
+    // } 
+    // cout<<maxnum; 
+} 
